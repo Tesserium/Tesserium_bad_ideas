90a68cc4f980e3a6b74a7302e081ac45cbd70ede
Haocheng Zhang <50510911+code2828@users.noreply.github.com>
2025-07-14 20:55:01 +0800

    Create stub random_dict.cpp
    Complete later when I have time

---
+++ random_dict.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool bucket[300]={0};
+
+int is_variant_of_random(string modnar)
+{
+    // stub
+    // input: a string.
+    // output: 0 = not a variant of random, 1 = a variant of random, other = error occured
+    // if a string contains more than 4 out of the 6 letters of r a n d o m, it is considered to be a variant of random
+    // use the bucket array to count
+  
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
