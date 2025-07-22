b5408639510f16d25a4666e6979217f3c13ef246
code2828 <stevenhaocheng@163.com>
2025-07-22 11:12:06 +0000

    is_variant_of_random

--- random_dict.cpp
+++ random_dict.cpp
@@ -2,7 +2,8 @@
 #include<string>
 using namespace std;
 
-bool bucket[300]={0};
+int bucket[300] = {0};
+int random_count = 0;
 
 int is_variant_of_random(string modnar)
 {
@@ -11,11 +12,22 @@ int is_variant_of_random(string modnar)
     // output: 0 = not a variant of random, 1 = a variant of random, other = error occured
     // if a string contains more than 4 out of the 6 letters of r a n d o m, it is considered to be a variant of random
     // use the bucket array to count
-  
+    for(int i = 0; i < modnar.length(); i++)
+        bucket[modnar[i]]++;
+    if(modnar['r'] + modnar['a'] + modnar['n'] + modnar['d'] + modnar['o']
+        + modnar['m'] >= 4)
+        cout << random_count++ << ": " << modnar << endl;
+
     return 0;
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    int n = 1;
+    while(n < argc)
+    {
+        int ret = is_variant_of_random(argv[n++]);
+    }
+
     return 0;
 }
